ace3a091d5db31d4d8deea04449ccdffcd26998f
Louis Vichy <lgvichy@gmail.com>
2016-02-12 13:42:29 -0800

    Some twerk to the new api to fetch the real thing

--- www/com/home/home.js
+++ www/com/home/home.js
@@ -1,19 +1,20 @@
-'use strict()';
+"use strict()";
 
 var conceptsAPI = "./com/home/db/concepts.json";
-var annotationsAPI = "./com/home/db/annotations.json";
+var keywordsAPI = "./com/home/db/keywords.json";
 var personalityAPI = "./com/home/db/personality.json";
 var toneAPI = "./com/home/db/tone.json";
+var analyzerAPI = "./com/home/db/analyzed.json"
 
 function HomeCtrl($http) {
-  console.log('HomeCtrl');
+  console.log("HomeCtrl");
 
   var home = this;
 
   home.idea = {
     raw: "", // Raw idea expression
     pitch: "", // The pitch
-    annotations: null, // All keywords extractable
+    keyData: null, // All keywords extractable
     related: null, // All related concepts, expecting about 30 mores of them
     product: "",
     market: "",
@@ -22,20 +23,20 @@ function HomeCtrl($http) {
   };
 
   home.getRandomIdea = function () {
-    $http.jsonp('http://itsthisforthat.com/api.php?call=JSON_CALLBACK')
+    $http.jsonp("https://itsthisforthat.com/api.php?call=JSON_CALLBACK")
       .success(function (data) {
         console.log(data);
 
         home.idea.product = data.this;
         home.idea.market = data.that;
 
-        home.idea.raw = "So, Basically, It's Like A " + data.this + " for " + data.that + ".";
+        home.idea.raw = "So, Basically, It's Like " + data.this + " for " + data.that + ".";
         console.log(home.idea.raw);
       });
   };
 
   home.getRelatedConcepts = function () {
-    // $http.get('https://id34.mybluemix.net/a/' + home.idea.raw)
+    // $http.get("https://id34.mybluemix.net/a/" + home.idea.raw)
     $http.get(conceptsAPI)
       .then(
         function (response) {
@@ -48,13 +49,15 @@ function HomeCtrl($http) {
       )
   };
 
-  home.getAnnotations = function () {
-    // $http.get('https://id34.mybluemix.net/a/' + home.idea.raw)
-    $http.get(annotationsAPI)
+  home.getKeywords = function () {
+    console.log("GET KEY");
+
+    // $http.get("https://id34.mybluemix.net/e/" + encodeURIComponent(home.idea.raw))
+    $http.get(keywordsAPI)
       .then(
         function (response) {
-          console.log(response.data);
-          home.idea.annotations = response.data;
+          console.log(response);
+          home.idea.keyData = response.data;
         },
         function (err) {
           console.log(err);
@@ -72,12 +75,12 @@ function HomeCtrl($http) {
 
           var raw = response.data.tree.children[0].children[0].children;
 
-          home.labels = raw.map(function(a) {
+          home.labels = raw.map(function (a) {
             return a.id;
           });
 
-          home.data = raw.map(function(a) {
-            if(a.percentage){
+          home.data = raw.map(function (a) {
+            if(a.percentage) {
               return a.percentage * 100;
             }
           });
@@ -90,6 +93,7 @@ function HomeCtrl($http) {
   };
 
   home.getTone = function () {
+    // $http.get("https://id34.mybluemix.net/pa/" + encodeURIComponent(home.idea.raw))
     $http.get(toneAPI)
       .then(
         function (response) {
@@ -102,11 +106,16 @@ function HomeCtrl($http) {
       )
   };
 
-  function analyzeIdea() {
-    $http.get('https://id34.mybluemix.net/ai/' + home.idea)
+  home.analyzeIdea = function () {
+    home.getTone();
+
+    // $http.get("https://id34.mybluemix.net/ai/" + encodeURIComponent(home.idea.raw))
+    $http.get(analyzerAPI)
       .then(
         function (response) {
           console.log(response.data);
+          home.idea.personality = response.data.profile;
+          home.idea.related = response.data.conceptCollection[0];
         },
         function (err) {
           console.log(err);
@@ -118,10 +127,9 @@ function HomeCtrl($http) {
 
   //* //////////////////////////////////////////////
 
-  home.getAnnotations();
-  home.getRelatedConcepts();
-  home.getTone();
-  home.getPersonality();
+  // home.getRelatedConcepts();
+  // home.getTone();
+  // home.getPersonality();
 
   //*/////////////////////////////////////////////
 
