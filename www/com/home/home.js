cd0ebf7861877868b5b02c3718923f05e0ba03f8
Louis Vichy <lgvichy@gmail.com>
2016-02-12 01:25:52 -0800

    Init the client side project

---
+++ www/com/home/home.js
@@ -0,0 +1,34 @@
+'use strict()';
+
+var memeAPI = "./com/home/meme.json";
+
+function HomeCtrl($scope, $interval, $http, $ionicPopup, $timeout) {
+  console.log('HomeCtrl');
+
+  $scope.$on('$ionicView.enter', function (e) {
+    $http.get(memeAPI)
+      .then(function (resp) {
+        $scope.memes =
+          // resp.data.data.memes;
+          resp.data;
+        console.log($scope.memes);
+      })
+  });
+
+  $scope.getRandomIdea = function () {
+    $http({
+        method: 'GET',
+        url: 'http://itsthisforthat.com/api.php?json'
+      })
+      .then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(response);
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+  }
+
+
+}
