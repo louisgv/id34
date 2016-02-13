8099604174b2836bc15bf2c62e21891768a953a0
Louis Vichy <lgvichy@gmail.com>
2016-02-12 18:27:57 -0800

    Safe to have the tone live as it's beta

--- www/com/home/home.js
+++ www/com/home/home.js
@@ -6,7 +6,7 @@ var personalityAPI = "./com/home/db/personality.json";
 var toneAPI = "./com/home/db/tone.json";
 var analyzerAPI = "./com/home/db/analyzed.json"
 
-function HomeCtrl($http) {
+function HomeCtrl($http, $ionicLoading) {
   console.log("HomeCtrl");
 
   var home = this;
@@ -16,10 +16,9 @@ function HomeCtrl($http) {
     pitch: "", // The pitch
     keyData: null, // All keywords extractable
     related: null, // All related concepts, expecting about 30 mores of them
-    product: "",
-    market: "",
     tone: null,
-    personality: null
+    personality: null,
+    conceptCollection: null
   };
 
   home.getRandomIdea = function () {
@@ -27,8 +26,8 @@ function HomeCtrl($http) {
       .success(function (data) {
         console.log(data);
 
-        home.idea.product = data.this;
-        home.idea.market = data.that;
+        // home.idea.product = data.this;
+        // home.idea.market = data.that;
 
         home.idea.raw = "So, Basically, It's Like " + data.this + " for " + data.that + ".";
         console.log(home.idea.raw);
@@ -65,37 +64,10 @@ function HomeCtrl($http) {
       )
   };
 
-  home.getPersonality = function () {
-    $http.get(personalityAPI)
-      .then(
-        function (response) {
-          console.log("Personality ---------------");
-          console.log(response.data.tree.children[0].children[0].children);
-          home.idea.personality = response.data;
-
-          var raw = response.data.tree.children[0].children[0].children;
-
-          home.labels = raw.map(function (a) {
-            return a.id;
-          });
-
-          home.data = raw.map(function (a) {
-            if(a.percentage) {
-              return a.percentage * 100;
-            }
-          });
-
-        },
-        function (err) {
-          console.log(err);
-        }
-      )
-  };
-
   home.getTone = function () {
-    // $http.get("https://id34.mybluemix.net/pa/" + encodeURIComponent(home.idea.raw))
-    $http.get(toneAPI)
+    $http.get("https://id34.mybluemix.net/pa/" + encodeURIComponent(home.idea.raw))
       .then(
+        // $http.get(toneAPI)
         function (response) {
           console.log(response.data);
           home.idea.tone = response.data;
@@ -107,6 +79,11 @@ function HomeCtrl($http) {
   };
 
   home.analyzeIdea = function () {
+    $ionicLoading.show({
+      template: '<ion-spinner icon="ripple" class="spinner-energized"></ion-spinner><br>Analyzing your possible future',
+      animation: 'fade-in'
+    });
+
     home.getTone();
 
     // $http.get("https://id34.mybluemix.net/ai/" + encodeURIComponent(home.idea.raw))
@@ -114,8 +91,12 @@ function HomeCtrl($http) {
       .then(
         function (response) {
           console.log(response.data);
+
           home.idea.personality = response.data.profile;
-          home.idea.related = response.data.conceptCollection[0];
+
+          home.idea.conceptCollection = response.data.conceptCollection;
+
+          $ionicLoading.hide();
         },
         function (err) {
           console.log(err);
